3a20a671e782263b23327bcfa05d508a304d4260
Michel Gaudry <agilare@gmail.com>
2023-02-10 18:43:25 +0100

    fix(mailing): handle fails of PHPMailer

--- librairies/Utils/Mailing.php
+++ librairies/Utils/Mailing.php
@@ -5,12 +5,13 @@ namespace Ladecadanse\Utils;
 use PHPMailer\PHPMailer\PHPMailer;
 
 class Mailing {
-    
+
     private $mail;
-    
-     public function __construct()
+    private $errorMsg = "Message could not be sent. Mailer Error: ";
+
+    public function __construct()
      {
-        $this->mail = new PHPMailer(); 
+        $this->mail = new PHPMailer();
         $this->mail->SMTPDebug = (int)EMAIL_AUTH_SMTPDEBUG;
         $this->mail->isSMTP();
         $this->mail->Host = EMAIL_AUTH_HOST;
@@ -18,57 +19,66 @@ class Mailing {
         $this->mail->Username = EMAIL_AUTH_USERNAME;
         $this->mail->Password = EMAIL_AUTH_PASSWORD;
         $this->mail->SMTPSecure = EMAIL_AUTH_SMTPSECURE;
-        $this->mail->Port = (int)EMAIL_AUTH_PORT;        
-        $this->mail->CharSet = 'utf-8';    
+        $this->mail->Port = (int)EMAIL_AUTH_PORT;
+        $this->mail->CharSet = 'utf-8';
      }
-     
+
     public function toAdmin(string $title, string $body, ?string $from): bool
     {
         $this->mail->From     = EMAIL_SITE;
-        $this->mail->FromName = EMAIL_SITE_NAME;     
+        $this->mail->FromName = EMAIL_SITE_NAME;
         if (!empty($from))
         {
             $this->mail->From     = $from;
-            $this->mail->FromName = '';                
+            $this->mail->FromName = '';
         }
-          
+
         $this->mail->Subject 	= "[La décadanse] ".$title;
         $this->mail->Body = $body;
         $this->mail->AddAddress(EMAIL_ADMIN, EMAIL_ADMIN_NAME);
-        try {        
-            $this->mail->Send();
-            return true;
+        try
+        {
+            if (!$this->mail->Send()) {
+                echo $this->errorMsg . $this->mail->ErrorInfo;
+                return false;
+            }
         } catch (\Exception $e) {
-            echo "Message could not be sent. Mailer Error: {$this->mail->ErrorInfo}";
+            echo $this->errorMsg . $this->mail->ErrorInfo;
             return false;
-        }              
-    } 
-    
+        }
+
+        return true;
+    }
+
     public function toUser(string $to, string $title, string $body, array $replyTo = [], ?string $attachementPath = null): bool
     {
         $this->mail->From     = EMAIL_SITE;
         $this->mail->FromName = EMAIL_SITE_NAME;
-        
+
         if (!empty($replyTo))
         {
             $this->mail->addReplyTo($replyTo['email'], $replyTo['name']);
         }
-        
+
         $this->mail->Subject = $title;
         $this->mail->Body = $body;
-        
+
         if (!empty($attachementPath))
         {
             $this->mail->addAttachment($attachementPath);
         }
-        
+
         $this->mail->AddAddress($to, "");
-        try {        
-            $this->mail->Send();
-            return true;
+        try {
+            if (!$this->mail->Send()) {
+                echo $this->errorMsg . $this->mail->ErrorInfo;
+                return false;
+            }
         } catch (\Exception $e) {
-            echo "Message could not be sent. Mailer Error: {$this->mail->ErrorInfo}";
+            echo $this->errorMsg . $this->mail->ErrorInfo;
             return false;
-        }  
-    } 
+        }
+
+        return true;
+    }
 }
