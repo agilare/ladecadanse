c6b924f4561357444f0bf64d6b655ff890a1e00e
Michel Gaudry <agilare@gmail.com>
2023-02-11 15:06:53 +0100

    refactor(organisateurs): simplify home

--- lieux.php
+++ lieux.php
@@ -8,11 +8,11 @@ use Ladecadanse\Utils\Validateur;
 
 if ($_SESSION['region'] == 'vd')
  {
-     $page_titre_region = " à Lausanne";   
+     $page_titre_region = " à Lausanne";
  }
  elseif ($_SESSION['region'] == 'fr')
  {
-     $page_titre_region = " à Fribourg";   
+     $page_titre_region = " à Fribourg";
  }
  else
  {
@@ -21,124 +21,24 @@ if ($_SESSION['region'] == 'vd')
 
 $page_titre = "Lieux de sorties ".$page_titre_region." : bistrots, salles, bars, restaurants, cinémas,
  théâtres, galeries, boutiques, musées, ...";
-$page_description = "Dernières descriptions de lieux à Genève";
+$page_description = "";
 $extra_css = array("menu_lieux");
 include("_header.inc.php");
 
 $get['idL'] = "";
 if (isset($_GET['idL']))
 {
-try {    
+try {
 	$get['idL'] = Validateur::validateUrlQueryValue($_GET['idL'], "int", 0);
- } catch (Exception $e) { header($_SERVER["SERVER_PROTOCOL"]." 400 Bad Request"); exit; }    
+ } catch (Exception $e) { header($_SERVER["SERVER_PROTOCOL"]." 400 Bad Request"); exit; }
 }
 
 $fiches = new DescriptionCollection();
 $fiches->loadFiches('description', $_SESSION['region']);
 $pair = 0;
-
-$map_style = '';
-if ((isset($_SESSION['Sgroupe']) && $_SESSION['Sgroupe'] <= 4))
-{
-    
-
-$rf = '';
-if ($_SESSION['region'] == 'ge')
-    $rf = "'rf',";
-    
-//  AND e.statut = 'actif' AND e.dateEvenement >= CURDATE()
-$sql_lieux = "
-SELECT l.idLieu AS idLieu, nom, l.adresse AS adresse, l.quartier AS quartier, categorie, lat, lng, (select count(idEvenement) from evenement e where e.statut = 'actif' AND e.dateEvenement >= CURDATE() and e.idLieu = l.idLieu) AS nb_event
-FROM lieu l
-WHERE l.statut='actif' AND l.region IN ('".$connector->sanitize($_SESSION['region'])."', $rf 'hs')  
-    AND lat != '0.000000' AND lng != '0.000000'
-ORDER BY TRIM(LEADING 'l\'' FROM (TRIM(LEADING 'les ' FROM (TRIM(LEADING 'la ' FROM (TRIM(LEADING 'le ' FROM lower(nom)))))))) COLLATE utf8mb4_unicode_ci";
-
-//echo $sql_lieux;
-
-$req_lieux = $connector->query($sql_lieux);
-
-$nb_lieux = $connector->getNumRows($req_lieux);
-
-$pair = 0;
-$prec = "";
-$url_prec = "";
-$url_suiv = "";
-$nomDuLieu = "";
-$id_passe = 0;
-
-$tab_markers = [];
-while ($lieu = mysqli_fetch_assoc($req_lieux))
-{
-    $tab_markers[] = $lieu;
-}
 ?>
-
-<script type="text/javascript">
-
-var tab_markers = <?php echo json_encode($tab_markers); ?>;
-
-function initMap() {
-	return;
-	var mapDiv = document.getElementById('map');
-	var map = new google.maps.Map(mapDiv, {
-	  center: {lat: 46.519653, lng: 6.632273}, // Suisse Romande
-	  zoom: 9
-	});
-		
-	markers = [];
-	infowindows = [];
-	
-	var bounds = new google.maps.LatLngBounds();
-	
-	jQuery.each(tab_markers, function() {
-				
-		// création position avec lat et lng, puis marker dans map, puis extension contours avec ce nouv. marker
-		var pos = new google.maps.LatLng(parseFloat(this['lat']), parseFloat(this['lng'])); 
-		var marker = new google.maps.Marker({
-			position: pos,
-           label: this['nb_event'],
-
-			map: map
-           // icon: {                               url: "http://maps.google.com/mapfiles/ms/icons/blue-dot.png"                           }
-		});
-		
-		bounds.extend(marker.getPosition());
-		
-		var contentString = '<div class="vd-marker-infowindow"><a href="/lieu.php?idL=' + this['idLieu'] + '"><b>' + this['nom'] + '</b></a><br>' + this['categorie'] + '<br>' + this['adresse'] + '<br>' + this['quartier'];
-		
-		var infowindow = new google.maps.InfoWindow({
-			content: contentString
-		});
-		
-		infowindows.push(infowindow);
-
-		marker.addListener('click', function() {
-			
-		   for (var i = 0; i < infowindows.length; i++) {
-			  infowindows[i].close();
-			}
-			
-			infowindow.open(map, marker);
-		});
-		
-		markers.push(marker);
-		map.fitBounds(bounds);
-	});
-	
-	//var mc = new MarkerClusterer(map, markers, {styles: [{  textColor: "#e72557", textSize:13, height:46, width:46, url: '/templates/ee/images/cluster-j.png' }], gridSize: 40, maxZoom: 15}); // anchor:[14,16],
-
-  }
-
-  
-  
-</script>
-<?php 
-$map_style = 'style="width:94%;margin:0 auto;height:500px;border:1px solid #555;border-radius:4px;display:none"';
-
-} ?>
 <div id="contenu" class="colonne">
-    
+
     <div id="entete_contenu">
         <h2 style="font-size:1.6em; width: 15%;">Lieux</h2> <?php if (isset($_SESSION['Sgroupe']) && $_SESSION['Sgroupe'] <= 6) { ?><a href="/lieu-edit.php?action=ajouter" style="float: left;padding: 5px 1px;"><img src="/web/interface/icons/building_add.png" alt=""  /> Ajouter un lieu</a><?php } ?><?php HtmlShrink::getMenuRegions($glo_regions, $get); ?>
         <div class="spacer"></div>
@@ -147,15 +47,12 @@ $map_style = 'style="width:94%;margin:0 auto;height:500px;border:1px solid #555;
         </p>
     </div>
 
-	<div class="spacer"></div>
-    
-    
-    <div id="map" <?php echo $map_style ?>></div>
- <div style="clear:both"></div>
-	
+    <div class="spacer"></div>
+    <div style="clear:both"></div>
+
     <div id="derniers_lieux" style="width:94%;margin:0 auto;">
 
-	<h2>Derniers lieux ajoutés</h2>
+        <h2>Derniers lieux ajoutés</h2>
 
 	<?php
 	$req_lieux_recents = $connector->query("
@@ -171,9 +68,9 @@ $map_style = 'style="width:94%;margin:0 auto;height:500px;border:1px solid #555;
 		//printr($tab_lieux_recents);
 		?>
 		<h3><a href="/lieu.php?idL=<?php echo $tab_lieux_recents['idLieu']; ?>" title="Voir la fiche du lieu" ><?php echo $tab_lieux_recents['nom']; ?></a></h3>
-		
-		<p><?php 
-                
+
+		<p><?php
+
                 echo htmlspecialchars(HtmlShrink::getAdressFitted( '', $tab_lieux_recents['localite'], $tab_lieux_recents['quartier'], $tab_lieux_recents['adresse'])); ?></p>
 		<?php
 		}
@@ -181,7 +78,7 @@ $map_style = 'style="width:94%;margin:0 auto;height:500px;border:1px solid #555;
 	?>
 
 	</div>
-   
+
 
 <div class="clear_mobile"></div>
 </div>
--- organisateurs.php
+++ organisateurs.php
@@ -4,19 +4,18 @@ require_once("app/bootstrap.php");
 
 use Ladecadanse\OrganisateurCollection;
 use Ladecadanse\Utils\Validateur;
-use Ladecadanse\Utils\Text;
 
 $page_titre = "Organisateurs d'événements culturels à Genève et Lausanne : associations, labels, collectifs";
-$page_description = "Derniers organisateurs ajoutés";
+$page_description = "";
 $extra_css = array("menu_lieux");
 include("_header.inc.php");
 
 $get['idO'] = "";
 if (isset($_GET['idO']))
 {
-    try { 
+    try {
         $get['idO'] = Validateur::validateUrlQueryValue($_GET['idO'], "int", 0);
-    } catch (Exception $e) { header($_SERVER["SERVER_PROTOCOL"]." 400 Bad Request"); exit; }        
+    } catch (Exception $e) { header($_SERVER["SERVER_PROTOCOL"]." 400 Bad Request"); exit; }
 }
 
 /**
@@ -28,68 +27,32 @@ $pair = 0;
 ?>
 
 <div id="contenu" class="colonne">
-    
+
     <div id="entete_contenu">
         <h2  style="font-size:1.6em; width: 30%;">Organisateurs</h2><?php if (isset($_SESSION['Sgroupe']) && $_SESSION['Sgroupe'] <= 6) { ?><a href="/organisateur-edit.php?action=ajouter" style="float: right;padding: 5px 1px;"><img src="/web/interface/icons/add.png" alt="" style="vertical-align:bottom" /> Ajouter un organisateur</a><?php } ?>
         <div class="spacer"></div>
         <p class="mobile" id="btn_listelieux">
             <button href="#"><i class="fa fa-list fa-lg"></i>&nbsp;Liste des organisateurs</button>
-        </p>        
+        </p>
     </div>
 
     <div class="spacer"></div>
-    
-    <ol id="dernieres_descriptions">
 
-    <?php
-    foreach ($col->getElements() as $id => $fiche)
-    {
+    <div id="derniers_lieux" style="width:94%;margin:0 auto;">
 
-        $photo_principale = '';
-        if ($fiche->getValue('logo') != "")
+        <h2 style="">Derniers organisateurs ajoutés</h2>
+
+        <?php
+        foreach ($col->getElements() as $id => $fiche)
         {
-            $logo_time = @filemtime($rep_uploads_organisateurs.$fiche->getValue('logo'));
-            $photo_principale = "<a href=\"/organisateur.php?idO=".$fiche->getValue('idOrganisateur')."\" title=\"Voir la fiche de l'organisateur : ".sanitizeForHtml($fiche->getValue('nom'))."\">
-            <img src=\"".$url_uploads_organisateurs.$fiche->getValue('logo')."?".$logo_time."\" width=\"100\" alt=\"".sanitizeForHtml($fiche->getValue('nom'))."\" /></a>\n";
-        }
-
-        //Réduction du descriptif
-        $maxChar = Text::trouveMaxChar($fiche->getValue('presentation'), 36, 8);
-        $tailleCont = mb_strlen($fiche->getValue('presentation'));
-
-        $apercu = $fiche->getValue('presentation');
-        if ($tailleCont > $maxChar)
-        {
-            $apercu = Text::html_substr($fiche->getValue('presentation'), $maxChar, 2);
-        }
-        ?>
-
-        <!-- Début vignette -->
-        <li class="vignette<?php if ($pair % 2 != 0){echo " ici";} ?>">
-
-            <div class="icone">
-            <?php echo $photo_principale; ?>
-            </div>
+            ?>
             <h3><a href="/organisateur.php?idO=<?php echo $fiche->getValue('idOrganisateur'); ?>" title="Voir la fiche de l'organisateur : <?php echo sanitizeForHtml($fiche->getValue('nom')); ?>"><?php echo sanitizeForHtml($fiche->getValue('nom')); ?></a></h3>
-            <span class="qui"><?php echo date_fr($fiche->getValue('date_ajout'), "annee", "non", "non"); ?></span>
-            <div class="spacer"></div>
-            <div class="apercu">
-            <?php echo $apercu; ?>
-            </div>
-            <div class="continuer">
-                <a href="/organisateur.php?idO=<?php echo $fiche->getValue('idOrganisateur'); ?>" title="Voir la fiche de l'organisateur  : <?php echo sanitizeForHtml($fiche->getValue('nom')); ?>">
-            Voir la fiche complète</a>
-            </div>
-        </li>
-        <!-- FIN vignette -->
-    <?php
-        $pair++;
-
+            <?php
     } // while
     ?>
 
-	</ol>
-	<!-- Fin dernieres_descriptions -->
+    </div>
+    <!-- Fin dernieres_descriptions -->
 
 </div>
 <!-- fin Contenu -->
