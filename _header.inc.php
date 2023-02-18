46828ad486262fb6afb785b4578c32544c594bed
Michel Gaudry <agilare@gmail.com>
2023-02-18 13:03:59 +0100

    chore(stats): update Matomo tracking for tools.ladecadanse.ch/matomo

--- _header.inc.php
+++ _header.inc.php
@@ -78,22 +78,22 @@ use Ladecadanse\HtmlShrink;
     <?php
     if (MATOMO_ENABLED) {
     ?>
-    <!-- Matomo -->
-    <script type="text/javascript">
-      var _paq = _paq || [];
-      // tracker methods like "setCustomDimension" should be called before "trackPageView"
-      _paq.push(['trackPageView']);
-      _paq.push(['enableLinkTracking']);
-      (function() {
-        var u="//ladecadanse.darksite.ch/piwik/";
-        _paq.push(['setTrackerUrl', u+'piwik.php']);
-        _paq.push(['setSiteId', '1']);
-        var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-        g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
-      })();
-    </script>
-    <!-- End Matomo Code -->
-    <?php
+        <!-- Matomo -->
+        <script>
+          var _paq = window._paq = window._paq || [];
+          /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+          _paq.push(['trackPageView']);
+          _paq.push(['enableLinkTracking']);
+          (function() {
+            var u="//tools.ladecadanse.ch/matomo/";
+            _paq.push(['setTrackerUrl', u+'matomo.php']);
+            _paq.push(['setSiteId', '1']);
+            var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+            g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+          })();
+        </script>
+        <!-- End Matomo Code -->
+        <?php
     }
     ?>
     <?php
