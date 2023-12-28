8c32729009e1944f56a01eb38313ddb3e2f791b3
Michel Gaudry <agilare@gmail.com>
2023-12-28 20:30:05 +0100

    chore(js): add eslint, as Node package

---
+++ .eslintrc.js
@@ -0,0 +1,44 @@
+module.exports = {
+    root : true,
+    "env": {
+        "browser": true,
+        "es2018": true,
+        "jquery": true //https://stackoverflow.com/a/39511584/1410592
+    },
+    "extends": "eslint:recommended",
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};
