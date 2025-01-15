edf8b4fd6215d0e951771d8a0eff847eb10f87a9
Michel Gaudry <agilare@gmail.com>
2025-01-15 20:02:28 +0100

    impr(content): add womeninthecity-geneva.ch in Links page

--- articles/liens.php
+++ articles/liens.php
@@ -17,9 +17,10 @@ include("../_header.inc.php");
 	<div class="rubrique">
 
         <ul>
-            <li><a href="http://www.raggasessions.ch/" target="_blank">Raggasessions.ch</a> : agenda de concerts et soirées reggae en Suisse</li>
-            <li><a href="http://reprezent.ch/" target="_blank">Reprezent</a> : actualité du hip hop en Suisse Romande</li>
+            <li><a href="https://www.raggasessions.ch/" target="_blank">Raggasessions.ch</a> : agenda de concerts et soirées reggae en Suisse</li>
+            <li><a href="https://reprezent.ch/" target="_blank">Reprezent</a> : actualité du hip hop en Suisse Romande</li>
             <li><a href="http://azanya.ch/" target="_blank">Azanya.ch</a> : l’Agenda des événements culturels Afro-Caribéens de Genève et environs</li>
+            <li><a href="https://womeninthecity-geneva.ch/" target="_blank">womeninthecity-geneva.ch - Balade d'un autre genre</a> :  partir sur les traces de femmes qui ont marqué l’Histoire de la Cité via une application mobile téléchargeable gratuitement</li>
         </ul>
 
     </div>
