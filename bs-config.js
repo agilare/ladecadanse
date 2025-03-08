3e51eb6f8e7e434b00e936e3217b33f5f1287b6e
Michel Gaudry <agilare@gmail.com>
2025-03-08 16:31:17 +0100

    chore(dev): browser-sync config clearer

--- bs-config.js
+++ bs-config.js
@@ -1,8 +1,9 @@
 module.exports = {
-    proxy: "http://ladecadanse.local",
+    proxy: "ladecadanse.local",
     files: [
-        "**/*.php",            // Tous les fichiers PHP dans www et ses sous-répertoires
-        "web/**/*.css",        // Tous les fichiers CSS dans public et sous-dossiers
-        "web/**/*.js"          // Tous les fichiers JS dans public et sous-dossiers
-    ]
+        "**/*.php",            // Tous les fichiers PHP dans la racine et ses sous-répertoires
+        "web/**/*.css",        // Tous les fichiers CSS dans 'web' et sous-répertoires
+        "web/**/*.js"          // Tous les fichiers JS dans 'web' et sous-répertoires
+    ],
+    index: "index.php"
 };
