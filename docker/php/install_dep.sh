17982e455479f8fc5ba7370c731b8d70020f1898
VonUniGE <17067975+VonUniGE@users.noreply.github.com>
2025-03-10 21:35:32 +0100

    refactor(docker): avoid to download composer.phar

---
+++ docker/php/install_dep.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+php ./docker/php/composer.phar install
--- install_dep.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-php composer-setup.php install
-php composer.phar install
