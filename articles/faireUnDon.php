af148b49fc56a4ab263fd91008ce746dc4d925a6
Michel Gaudry <agilare@gmail.com>
2025-05-30 16:10:44 +0200

    impr(donate): move description below the payment methods, text

--- articles/faireUnDon.php
+++ articles/faireUnDon.php
@@ -15,20 +15,9 @@ include("../_header.inc.php");
 
     <div class="rubrique" style="background:#f4f4f4;border-radius:3px;margin: 1em auto 0;padding:1em 1%;width: 92%;">
 
-        <p>La décadanse est un site entièrement gratuit sur un code <a href="https://github.com/agilare/ladecadanse/" target="_blank">open source</a>, que je développe et gère bénévolement depuis une vingtaine d'années, avec parfois l'aide d'autres personnes.</p>
+<!--        <p>Vous pouvez contribuer (une seule fois ou de manière récurrente, selon vos possibilités) via :</p>-->
 
-        <p>En dehors des frais qui sont assez modestes (hébergement, flyers...), le site me demande un certain investissement en temps :</p>
-        <ul style="margin-left:0.4em;list-style-type: none">
-            <li>✅ <b>maintenance</b> : assurer sa disponibilité, sa sécurité ; garder à jour, chasser les bugs</li>
-            <li>✅ <b>amélioration continue</b> : ajout de nouvelles fonctionnalités et optimisations</li>
-            <li>✅ <b>gestion régulière</b> : ajout des événements, lieux, etc. et leur mise à jour ; aide aux utilisateurs, modération</li>
-        </ul>
-        <br>
-        <p>Un <b>don</b>, quel que soit son montant, est une manière de <b>montrer votre soutien</b> et m’encourage à poursuivre ce projet !</p>
-
-        <p>Vous pouvez contribuer (une seule fois ou de manière récurrente, selon vos possibilités) via :</p>
-
-        <p><b>We make it</b> (privilégié) - Twint, virement, carte de crédit, Postfinance   <br>Compte à créer (rapide et permet de soutenir d'autres projets supers) :</p>
+        <p style="margin-bottom:0"><b>We make it</b> - Twint, virement, carte de crédit, Postfinance<br>Compte à créer (rapide et permet de soutenir d'autres projets supers) :</p>
         <script src="https://wemakeit.com/static-assets/widgets/donation_box.js" async="async"></script>
         <wemakeit-donation-box color="blue" locale="fr" slug="soutenir-la-decadanse" type="card" nonce="<?php echo CSP_NONCE ?>" style="margin: 1em 1em 1em 1em"></wemakeit-donation-box>
 <!--        <p>Carte de crédit(, Paypal)</p>-->
@@ -61,7 +50,19 @@ include("../_header.inc.php");
             <!--            <li><b>Twint, virement</b> (<a href="/contacteznous.php">contactez-moi</a>)</li>-->
         </ul>
 <br>
-        <!--        <p>Merci pour votre soutien ! ❤️</p>-->
+
+        <p>La décadanse est un site entièrement gratuit sur un code <a href="https://github.com/agilare/ladecadanse/" target="_blank">open source</a>, que je développe et gère bénévolement depuis une vingtaine d'années, avec parfois l'aide d'autres personnes.</p>
+
+        <p>En dehors des frais qui sont assez modestes (hébergement, flyers...), le site me demande un certain investissement en temps :</p>
+        <ul style="margin-left:0.4em;list-style-type: none">
+            <li>✅ <b>maintenance</b> : assurer sa disponibilité, sa sécurité ; garder à jour, chasser les bugs</li>
+            <li>✅ <b>amélioration continue</b> : ajout de nouvelles fonctionnalités et optimisations</li>
+            <li>✅ <b>gestion régulière</b> : ajout des événements, lieux, etc. et leur mise à jour ; aide aux utilisateurs, modération</li>
+        </ul>
+        <br>
+        <p>Un <b>don</b>, quel que soit son montant, est une manière de <b>montrer votre soutien</b> et m’encourage à poursuivre ce projet !</p>
+
+               <p>Merci pour votre aide ! ❤️</p>
 <p>Michel</p>
 
         <div class="spacer"></div>
