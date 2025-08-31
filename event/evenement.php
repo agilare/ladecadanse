25c8bf0d7b41d0eb358712e08ee8885983aa41a6
Michel Gaudry <agilare@gmail.com>
2025-08-31 13:19:31 +0200

    fix(event): in event page wrong link to agenda

--- event/evenement.php
+++ event/evenement.php
@@ -178,7 +178,7 @@ include("../_header.inc.php");
     <header id="entete_contenu">
 
         <div id="entete_contenu_titre" <?php if ($tab_even['e_dateEvenement'] < $glo_auj) { echo ' class="ancien"'; } ?>>
-            <span class="category"><?= sanitizeForHtml(ucfirst(Evenement::nom_genre($tab_even['e_genre']))); ?></span>, <a href="index.php?courant=<?= $tab_even['e_dateEvenement'] ?>"><time datetime="<?= $tab_even['e_dateEvenement'] ?>"><?= date_fr($tab_even['e_dateEvenement'], "annee", "", "", true) ?></time></a>
+            <span class="category"><?= sanitizeForHtml(ucfirst(Evenement::nom_genre($tab_even['e_genre']))); ?></span>, <a href="/index.php?courant=<?= $tab_even['e_dateEvenement'] ?>"><time datetime="<?= $tab_even['e_dateEvenement'] ?>"><?= date_fr($tab_even['e_dateEvenement'], "annee", "", "", true) ?></time></a>
         </div>
 
         <?php if (!empty($events_siblings[0])) : ?>
