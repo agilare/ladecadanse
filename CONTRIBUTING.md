3c9311717eea09b4867abea5c5ab021623cbe63f
Michel <agilare@gmail.com>
2026-01-12 20:49:23 +0100

    impr(contributing): add "bénévole", small texts improvements

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -29,13 +29,16 @@ Le **mode de développement actuel du projet** est brièvement décrit dans la s
 ### Tâches
 
 Vous pouvez reprendre des [Issues](https://github.com/agilare/ladecadanse/issues) existantes — en choisissant de préférences les plus prioritaires (label _high_) et/ou [suggérées](https://github.com/agilare/ladecadanse/wiki/Les-prochains-d%C3%A9veloppements-sugg%C3%A9r%C3%A9s) — ou en créer de nouvelles. Cela peut consister à :
+
 - spécifier, concevoir une amélioration
 - reproduire, cerner des bugs ou vulnérabilités
 - développer et tester
 
 Il est aussi possible de proposer des fonctionnalités, bien qu'en ce moment l'accent est mis surtout sur la résolution de _[bugs](https://github.com/agilare/ladecadanse/issues?q=is%3Aissue+is%3Aopen+label%3Abug)_ et le _[refactoring](https://github.com/agilare/ladecadanse/labels/refactoring)_ (notamment au sein des projets de modernisation) pour les raisons décrites ci-dessus.
 Si vous ne connaissez pas encore bien le code, vous pouvez commencer par une **_[Good first issue](https://github.com/agilare/ladecadanse/issues?q=is%3Aopen+is%3Aissue+label%3A%22good+first+issue%22)_**.
+
 Deux autres labels précisent leur domaine d'application :
+
 - _[improve-information](https://github.com/agilare/ladecadanse/labels/improve-information)_ : amélioration du contenu (sa diffusion, sa disponibilité)
 - _[edition](https://github.com/agilare/ladecadanse/labels/edition)_ : amélioration du "back-office", donc surtout pour les utilisateurs qui ajoutent des événements
 
--- articles/contributing.php
+++ articles/contributing.php
@@ -15,20 +15,20 @@ include("../_header.inc.php");
 
     <article class="rubrique">
 
-        <p>Merci de vous intéresser à ce projet qui est d&#39;une assez grande utilité pour <strong>faire connaître les événements de la région genevoise et ses environs</strong>. Les contributions sont bienvenues car il y a actuellement pas mal à faire, notamment dans la rénovation technique et visuelle.</p>
+        <p>Merci de vous intéresser à ce projet qui est d&#39;une assez grande utilité pour <strong>faire connaître les événements de la région genevoise et ses environs</strong>. Les contributions bénévoles sont bienvenues car il y a actuellement pas mal à faire, notamment dans la rénovation technique et visuelle.</p>
         <p>Vous pouvez aider de plusieurs manières :</p>
         <ul>
-        <li>🧑‍💻 participer à la gestion du <a href="https://www.ladecadanse.ch/">site actuel</a> (ajout d&#39;événements, aide, modération), ce qui me permettrait d&#39;avoir davantage de temps pour le développement (<a href="https://www.ladecadanse.ch/contacteznous.php">me contacter</a>)</li>
-        <li>🪙 <a href="https://www.ladecadanse.ch/articles/faireUnDon.php">faire un don</a> qui paierait les frais, soutiendrait mon travail</li>
+            <li>🧑‍💻 participer à la gestion du <a href="https://www.ladecadanse.ch/">site actuel</a> (ajout d&#39;événements, aide, modération), ce qui me permettrait d&#39;avoir davantage de temps pour le développement (<a href="https://www.ladecadanse.ch/contacteznous.php">me contacter</a>)</li>
+            <li>🪙 <a href="https://www.ladecadanse.ch/articles/faireUnDon.php">faire un don</a> qui paierait les frais, soutiendrait mon travail</li>
         </ul>
         <p>et spécifiquement, si vous êtes intéressé en tant que <strong>développeur</strong> :</p>
         <ul>
-        <li>⌨️ ajouter ou réaliser des <a href="https://github.com/agilare/ladecadanse/issues">Issues</a> (améliorations, refactoring, bugfixes...);
-            <br>👉 je propose une <a href="https://github.com/agilare/ladecadanse/wiki/Les-prochains-d%C3%A9veloppements-sugg%C3%A9r%C3%A9s">suggestion de développements utiles et intéressants</a></li>
-        <li>🔫 <a href="https://github.com/agilare/ladecadanse/blob/master/SECURITY.md">signaler voire corriger des vulnérabilités</a></li>
+            <li>⌨️ ajouter ou réaliser des <a href="https://github.com/agilare/ladecadanse/issues">Issues</a> (améliorations, refactoring, bugfixes...);
+                <br>👉 je propose une <a href="https://github.com/agilare/ladecadanse/wiki/Les-prochains-d%C3%A9veloppements-sugg%C3%A9r%C3%A9s">suggestion de développements utiles et intéressants</a></li>
+            <li>🔫 <a href="https://github.com/agilare/ladecadanse/blob/master/SECURITY.md">signaler voire corriger des vulnérabilités</a></li>
         </ul>
 
-        <p>La suite (pour les développeurs) sur le <a href="https://github.com/agilare/ladecadanse/blob/master/CONTRIBUTING.md#contexte" rel="external" target="_blank">CONTRIBUTING.md (contexte, développements, démarrage)</a></p>
+        <p>La suite, pour les développeurs, sur le <a href="https://github.com/agilare/ladecadanse/blob/master/CONTRIBUTING.md#contexte" rel="external" target="_blank">CONTRIBUTING.md</a> (contexte, développements, démarrage)</p>
 
     </article>
 
